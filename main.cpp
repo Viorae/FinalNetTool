f7b5d6d686505b7be234029e719548685eeabcea
unknown <1317114649@qq.com>
2022-04-04 11:08:51 +0800

    refine again

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     qApp->setStyle(QStyleFactory::create("Fusion"));
     //qApp->setStyleSheet("QTextEdit#textEdit{background-color: white;border: 2px solid black;border-radius:5px;}");
-    qApp->setStyleSheet("QFrame#frame{border: 2px solid black;background-color:#D1D0D0}");
+    //qApp->setStyleSheet("QFrame#frame{border: 0.5px solid black;background-color:#D1D0D0}");
     QApplication a(argc, argv);
     //qDebug()<<qApp->applicationDirPath()<<"   "<<qApp->applicationFilePath();
     Config::ConfigFile = QString("%1/config.ini").arg(qApp->applicationDirPath());
